b27b00b01c6b4aa551ff8a9a0ca313354f1a5777
Magdalene Mc <magdalener.mcarthur@gmail.com>
2016-11-09 20:26:47 +0000

    add runtime project

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     $.getJSON('data.json', function (data) {
         // YOUR CODE BELOW HERE //
+        $('#section-bio').css('border','1px').css('border-radius','5px').css('background-color', 'grey');
         
         // uncomment this to inspect all available data; delete when done //
         // console.log(data);
