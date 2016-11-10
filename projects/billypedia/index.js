466afe866504dd9a9deea59f88941a959f5292ef
Magdalene Mc <magdalener.mcarthur@gmail.com>
2016-11-10 01:54:19 +0000

    added billypedia project

---
+++ projects/billypedia/index.js
@@ -0,0 +1,23 @@
+/* global $ _ opspark */
+$(document).ready(function() {
+    $.getJSON('data.json', function (data) {
+        // YOUR CODE BELOW HERE //
+        $('#section-quotes').css('background-color', 'grey').css('border-radius', '4px');
+        let $section = $('<section>').attr('id', 'section-rider');
+        $section.append($('<h3>').text('Billy\'s Rider')).appendTo($('#sections'));
+        // uncomment this to inspect all available data; delete when done //
+        // console.log(data);
+        
+        // EXAMPLE: Looping over top rated recordings; replace with your code //
+        let topRated = data.discography.topRated;
+        _.forEach(topRated, function(recording) {
+            console.log(recording);
+        });
+        
+        
+        // YOUR CODE ABOVE HERE //
+    })
+    .fail(function() { console.log('getJSON on discography failed!'); });
+});
+
+
